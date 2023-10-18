f9402e22feb222259c468f927fa3ad6119148463
zulqarnain118 <ranamzulqarnain1@gmail.com>
2023-10-18 12:21:17 +0500

    done changes

--- vars/buildNode.groovy
+++ vars/buildNode.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call {
+def call() {
                echo "Building the application... for branch $BRANCH_NAME"
                 // Jenkins will use the globally installed Node.js and npm
                 sh 'npm install'
