fb911b2cf3ae014761b921f8a3a521ef59e8084b
zulqarnain118 <ranamzulqarnain1@gmail.com>
2023-10-18 12:13:41 +0500

    done changes

--- vars/buildDokcerImage.groovy
+++ vars/buildDokcerImage.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
 
-def call {
+def call(String imageName) {
                        echo 'Building the Docker Image...'
                 withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-                    sh 'docker build -t zulqarnain118/my-private-repo:1.3 .' 
+                    sh "docker build -t $imageName ."
                     sh "echo $PASS | docker login -u $USER --password-stdin"
-                    sh 'docker push zulqarnain118/my-private-repo:1.3' 
-
+                    sh "docker push $imageName"
                 }
 }
--- vars/buildNode.groovy
+++ vars/buildNode.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call {
-               echo 'Building the application...'
+               echo "Building the application... for branch $BRANCH_NAME"
                 // Jenkins will use the globally installed Node.js and npm
                 sh 'npm install'
 }
